1a318333446da333c28ebc030d3b4b5a1ab4cffb
Wanis Elabbar <wanis.elabbar@gamesysgroup.com>
2021-09-17 10:35:34 +0200

    fix

--- OSautomation/DSC/NewForest/NEWFOREST.ps1
+++ OSautomation/DSC/NewForest/NEWFOREST.ps1
@@ -1,9 +1,9 @@
 Configuration NewForest
 {
     ### AD Credentials from AWS SSM Parameters ###
-    $domain   = "{ssmtag:domainName}"
-    $username = "{ssmtag:domainJoinUsername}"
-    $password = "{ssmtag:domainJoinPassword}" | ConvertTo-SecureString -AsPlainText -Force
+    $domain   = '{ssmtag:domainName}'
+    $username = '{ssmtag:domainJoinUsername}'
+    $password = '{ssmtag:domainJoinPassword}' | ConvertTo-SecureString -AsPlainText -Force
     $credential = New-Object PSCredential($username, $password)
 
 
