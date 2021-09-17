5b7274bdcd6a82415f07c4f7e09c2ae26d9759a7
Wanis Elabbar <wanis.elabbar@gamesysgroup.com>
2021-09-17 08:59:52 +0200

    new forest test

---
+++ OSautomation/DSC/NewForest/NEWFOREST.ps1
@@ -0,0 +1,51 @@
+Configuration NewForest
+{
+    ### AD Credentials from AWS SSM Parameters ###
+    $domain = "{ssmtag:domainName}"
+    $username = "{ssmtag:domainJoinUsername}"
+    $password = "{ssmtag:domainJoinPassword}" | ConvertTo-SecureString -AsPlainText -Force
+    $credential = New-Object PSCredential($username, $password)
+
+    ### Import the necessary modules
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc -ModuleVersion 6.0.1
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain $domain
+        {
+            DomainName                    = $domain
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
+
+
+    $cd = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+            PSDscAllowPlainTextPassword = $true
+            PSDscAllowDomainUser = $true
+        }
+    )
+}
+
+
+NewForest -ConfigurationData $cd
+
+    
